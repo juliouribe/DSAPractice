6c6f7cf02693d57e048ce8cf816370d4da986bc7
Julio Uribe <uribejulioc@gmail.com>
2023-09-18 11:41:49 -0700

    Add validPalindrome

---
+++ exercises/strings/validPalindrome.js
@@ -0,0 +1,53 @@
+var isPalindromeV1 = function (s) {
+  if (s.length === 0 || s.length === 1) return true;
+  const arr = []
+  for (let i = 0; i < s.length; i++) {
+    let current = s[i].toLowerCase();
+    if (current.match(/[a-z]/i)) {
+      arr.push(current);
+    }
+  }
+  return arr.join(' ') === arr.reverse().join(' ');
+};
+
+var isPalindrome = function (s) {
+  // Check for palindrome dropping symbols and spaces. Case insensitive.
+  // Compare alphanumeric characters only.
+  
+  // Base case. A string is a palindrome if empty or just one character.
+  if (s.length === 0 || s.length === 1) return true;
+  // We're going to use two pointers starting at teh beginning and end
+  // We're going to scan towards the middle while checking for equality
+  // If we find a non alphabetic character we're going to skip it
+  // We're going to use regex to check for alphabetic characters
+
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    let leftChar = s[left].toLowerCase();
+    let rightChar = s[right].toLowerCase();
+    if (!leftChar.match(/[a-z0-9]/i)) {
+      left++;
+      continue;
+    }
+    if (!rightChar.match(/[a-z0-9]/i)) {
+      right--;
+      continue;
+    }
+    if (leftChar !== rightChar) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+let s = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(s)) // true
+
+s = "race a car";
+console.log(isPalindrome(s)) // false
+
+s = " ";
+console.log(isPalindrome(s)) // true
---
+++ notes/strings.js
@@ -0,0 +1,22 @@
+// ASCII contains 128 characters. Extended ASCII contains 256 characters.
+// Unicode contains 143,859 characters.
+
+abc = 'abc'
+
+// Use regex on a string to see if its alphanumeric
+abc[0].match(/[a-z0-9]/i) // true
+
+// Apply lowercase to a string
+abc[0].toLowerCase() // 'a'
+
+// Apply uppercase to a string
+abc[0].toUpperCase() // 'A'
+
+// Convert to array
+arr = abc.split('') // ['a', 'b', 'c']
+
+// Convert to string
+arr = abc.join('') // 'abc'
+
+// Reverse a string
+abc.split('').reverse().join('') // 'cba'
