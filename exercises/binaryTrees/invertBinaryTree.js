f5734e7372285d567d9b38c491feeae73ba63d01
Julio Uribe <uribejulioc@gmail.com>
2023-09-21 14:30:30 -0700

    Add invertBinaryTree

---
+++ exercises/binaryTrees/invertBinaryTree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  // start at root, swap the children
+  // go down left, swap the children
+  // go down right, swap the children
+  // keep going until this.left and this.right are null
+  // base case
+  if (root == null) {
+    return root;
+  }
+
+  invertTree(root.left);
+  invertTree(root.right);
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  return root;
+};
