bb1e2108422145580bb953063decd3769e1e02b9
Julio Uribe <uribejulioc@gmail.com>
2023-09-27 17:14:17 -0700

    Slightly better binary search

--- exercises/binarySearch/binarySearch.js
+++ exercises/binarySearch/binarySearch.js
@@ -4,20 +4,18 @@ const search = function (nums, target) {
   // if the target is larger, look at the right
   // Base cases
   if (nums.length === 0) return -1;
-  if (nums.length === 1) return nums[0] === target ? 0 : -1;
+  if (nums.length === 1) return nums[0] === target ? 0: -1;
 
-  const mid = Math.floor(nums.length / 2)
-  if (target === nums[mid]) {
-    return mid;
+  const mid = Math.floor(nums.length/2);
+  if (nums[mid] === target) {
+      return mid
   } else if (target < nums[mid]) {
-    // Look at the left side
-    const left = search(nums.slice(0, mid), target);
-    if (left > -1) return left;
+      return search(nums.slice(0, mid), target)
   } else {
-    // Look at the right side
-    const right = search(nums.slice(mid), target);
-    if (right > -1) return right + mid;
+      const result = search(nums.slice(mid + 1), target);
+      if (result > -1) {
+          return result + mid + 1;
+      }
+      return result;
   }
-
-  return -1;
 };
