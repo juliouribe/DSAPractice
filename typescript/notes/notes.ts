c915156882c6679cceed58030a3546a604e8842b
Julio Uribe <uribejulioc@gmail.com>
2024-01-22 09:35:58 -0800

    Add new TS folder and start exercises

---
+++ typescript/notes/notes.ts
@@ -0,0 +1,3 @@
+// Initialize an empty object with keys and values integer values (technically any number)
+const seen: {[key: number]: number} = {};
+
---
+++ typescript/twoSum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+  const seen: { [key: number]: number } = {};
+  for (let i = 0; i < nums.length; ++i) {
+    const value = nums[i];
+    const remainder = target - value;
+    if (remainder in seen) {
+      return [seen[remainder], i];
+    }
+    seen[value] = i;
+  }
+  return [];
+};
