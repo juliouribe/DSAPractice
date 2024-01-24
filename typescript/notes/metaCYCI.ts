0140a1589d5004ed121540e6a7e6049b02efbb57
Julio Uribe <uribejulioc@gmail.com>
2024-01-24 15:47:13 -0800

    Add Crush Your Coding Interview event notes

---
+++ typescript/notes/metaCYCI.ts
@@ -0,0 +1,61 @@
+/*
+Goals of a coding interview
+- how do you think and tackle hard problems
+- how do you consider engineering trade-offs (memory vs time)
+- how you communicate in English about technical problems
+- find the limits of what you know
+    - don't feel bad if you don't get all the answers right
+
+What is covered?
+- common problems in algorithms. No puzzle problems
+
+Key things to know about coding interviews
+- discuss complexity of space and time trade-offs
+- common library functions are fair game
+- specific questions about concepts are rare
+- when thinking about optimizing time vs space - consider both
+
+Strings
+- parsing and processing
+Arrays and ArrayLists/Vectors
+Queues and Stacks
+Maps, Hashmaps, and Sets
+Linked Lists
+- singly-linked, doubly-linked
+- circular
+Trees, BSTs, and N-ary trees
+- recursion
+- DFS vs BFS
+- tree traversal
+Graphs
+- directed vs undirected
+- DFS vs BFS
+- Adjacency List vs Matrix
+
+Format of a coding interview
+- don't worry if the interviewer stops you in the middle of a question. You might
+get interrupted even if it looks clear that you are going to solve.
+- Might have other follow ups they want to explore.
+- be prepared to test your code, discuss how it works, and analyze performance
+- phone screens use an online collaborative editor (but don't run any code!)
+- time to ask questions. During the Q&A you might want to use that time to figure
+out what you want from a career and figure out if Meta feels like a right fit
+
+Steps to a successful coding interview
+- repeat the question back to your interviewer to ensure you fully understand
+what's being asked. Remember, your questions may be purposefully vague!
+- ask questions about edge cases.
+- take your time to think about a solution before coding. Don't just jump into coding!
+- start coding while explaining your entire though process. It's encouraged to
+talk out loud. Don't worry if you run into trouble! Feel free to ask your interviewer
+for a hint. Interviewers may also give you hints unprompted, take them and run with them.
+- run through your solution with test cases. Continue talking out loud.
+- iterate - how can you do better with runtime/memory?
+
+
+
+Other Notes
+- we won't run the code. Think about how you would test your code.
+- focus on solving problems. Spitting out a class is most likely not the focus.
+
+*/
