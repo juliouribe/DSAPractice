41dc5cf1ef0b4caad3dc75129b1793c278359850
Julio Uribe <uribejulioc@gmail.com>
2024-02-03 18:53:12 -0800

    Work on courseSchedule

--- typescript/courseSchedule.ts
+++ typescript/courseSchedule.ts
@@ -1,27 +1,23 @@
-function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+function canFinish(numCourses: number, prereqs: number[][]): boolean {
     const adjList: number[][] = Array.from({ length: numCourses }, () => []);
 
-    for (let i = 0; i < prerequisites.length; i++) {
-        const vertex = prerequisites[i][0];
-        const edge = prerequisites[i][1];
-        adjList[vertex].push(edge);
+    // Go through node connections and update the adjacency list.
+    for (let i = 0; i < prereqs.length; i++) {
+        const [ start, target] = prereqs[i];
+        adjList[start].push(target);
     }
 
-    return topoSort(numCourses, adjList);
-};
-
-function topoSort(numCourses: number, adjList: number[][]): boolean {
     const indegreeEdge = new Array(numCourses).fill(0);
     const queue = [];
     const topoSort = [];
 
-    // Indegree
+    // Indegree. I think this is the number of courses that rely on a given course.
     for (let i = 0; i < numCourses; i++) {
-        for (let it of adjList[i]) {
-            indegreeEdge[it]++
+        for (let target of adjList[i]) {
+            indegreeEdge[target]++
         }
     }
-    // Zero incoming edge vertex;
+    // Zero incoming edge vertex. Courses with no prereqs so we are free to start with them.
     for (let i = 0; i < numCourses; i++) {
         if (indegreeEdge[i] === 0) {
             queue.push(i);
@@ -30,14 +26,19 @@ function topoSort(numCourses: number, adjList: number[][]): boolean {
 
     // TopoSort
     while (queue.length) {
+        // We are starting with nodes that have no prereqs.
         const node = queue.shift();
+        // We're only adding nodes with no dependencies to the topoSort array.
+        // If we don't add all courses into this array, then we'll have impossible courses.
         topoSort.push(node);
 
-        // Disconnect node
-        for (let it of adjList[node]) {
-            indegreeEdge[it]--;
-            if (indegreeEdge[it] === 0) {
-                queue.push(it)
+        // Disconnect node by looking up the nodes we're connected to and decrementing
+        // the indegree edges for each neighbor.
+        for (let target of adjList[node]) {
+            indegreeEdge[target]--;
+            // Once a neighbor has no more dependencies we can add it to the queue.
+            if (indegreeEdge[target] === 0) {
+                queue.push(target)
             }
         }
     }
