094f298efe5f3f83bbcf65af33bb37d978d5f651
Julio Uribe <uribejulioc@gmail.com>
2024-02-20 13:53:40 -0800

    Improve word search solution

--- typescript/wordSearch.ts
+++ typescript/wordSearch.ts
@@ -5,40 +5,43 @@ const dirs = [
     [0, -1]
 ]
 
-function dfs(
-    board: string[][], word: string, pos: number,
-    row: number, col: number): boolean {
-    // Assume we found the starting letter. Search in 4 directions for remaining letters
-    // Base Case
-    if ((row < 0 || row >= board.length) || (col < 0 || col >= board[0].length)) {
+function findWord(board: string[][], row: number, col: number, word: string, pos: number): boolean {
+    // Off map?
+    if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
         return false;
     }
-    // Not the letter we want.
+    // Board letter not equal to the next one
     if (board[row][col] !== word[pos]) {
         return false;
     }
-    // found the last letter
-    if ((board[row][col] === word[pos]) && ((word.length - 1) === pos)) {
+    // We have found all of the letters
+    if (board[row][col] === word[pos] && (word.length - 1) === pos) {
         return true;
     }
-    // Iterate over 4 dirs.
-    for (let dir of dirs) {
-        const [x, y] = dir;
-        const prev = board[row][col];
-        board[row][col] = '*';
-        if (dfs(board, word, pos + 1, row + y, col + x)) {
+    // Recurse step
+    const currChar = board[row][col];
+    board[row][col] = '_';
+    for (const [x, y] of dirs) {
+        if (findWord(board, row + y, col + x, word, pos + 1)) {
             return true;
         }
-        board[row][col] = prev;
     }
+    board[row][col] = currChar;
+
     return false;
 }
 
 function exist(board: string[][], word: string): boolean {
+    /*
+    Iterate over the board, top left to bot right.
+    Kick off DFS on the tile if it matches the first letter of the word.
+    DFS will look at the next character and keep going if it continues to find matches.
+    If we get to the end we return true.
+    */
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[0].length; col++) {
             if (board[row][col] === word[0]) {
-                if (dfs(board, word, 0, row, col)) {
+                if (findWord(board, row, col, word, 0)) {
                     return true;
                 }
             }
